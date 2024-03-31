af32d0b32b3f51e0e238708117432d66564024ec
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-04-01 01:37:53 +0530

    first commit

---
+++ src/main/java/com/demogradle/GradleProject.java
@@ -0,0 +1,7 @@
+package com.demogradle;
+
+public class GradleProject{
+    public static void main(String[] args){
+        System.out.println("Hello There!");
+    }
+}
---
+++ src/main/java/com/demogradle/test/java/com/demogradle/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package com.demogradle;
+
+import org.junit.Test;
+
+public class GradleTutorialTest{
+
+    @Test
+    public void verifyNoExceptionThrown(){
+        System.out.println("Hello There!");
+        GradleTutorial.main(new String[]{});
+    }
+}
